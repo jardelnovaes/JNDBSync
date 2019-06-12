5a4f88fa38ac81f8d8e93ce6855d05b50ddf1680
Jardel Novaes <jardelnovaes@hotmail.com>
2019-06-12 09:05:23 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off 
 cls
-java -cp libs;target/JNDBSync-0.0.1-SNAPSHOT.jar;D:/Neogrid/workspaces/tempWork/NeoDBSync/libs/ng-repository-api-0.9.2-nodeps.jar;D:/Neogrid/workspaces/tempWork/NeoDBSync/libs/sped-arch-1.60.0-SNAPSHOT.jar;D:/Neogrid/workspaces/tempWork/NeoDBSync/libs/sped-model-1.60.0-SNAPSHOT.jar; com.jardelnovaes.utils.database.neodbsync.JNDBSync
+java -cp libs;target/JNDBSync-0.0.1-SNAPSHOT.jar;D:/libs/repository-api-0.0.1.jar;D:/libs/model-1.0.0.jar; com.jardelnovaes.utils.database.neodbsync.JNDBSync
 
 pause
